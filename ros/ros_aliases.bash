60107fae8f9223824dd5246b50d30b2673f9889a
Tony Baltovski <tony.baltovski@gmail.com>
2015-04-10 09:41:15 -0400

    Update ros_aliases.bash

--- ros/ros_aliases.bash
+++ ros/ros_aliases.bash
@@ -1,5 +1,6 @@
 # ROS Aliases
 
 alias do_catkin_make='cd ~/catkin_ws; catkin_make -DCMAKE_BUILD_TYPE=Release'
+alias full_catkin_make='cd ~/catkin_ws; rm -rf build devel; catkin_make -DCMAKE_BUILD_TYPE=Release'
 alias rviz='rosrun rviz rviz'
 alias rqt_tf_tree='rosrun rqt_tf_tree rqt_tf_tree'
